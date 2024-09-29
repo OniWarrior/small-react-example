521804f0d16c17a2417a31edb92ad35fe0e115f3
Stephen Aranda <aranda.stephen88@gmail.com>
2024-09-29 18:56:10 -0500

    added title to home page

--- small-react-app/src/components/home.js
+++ small-react-app/src/components/home.js
@@ -1,9 +1,17 @@
 import React from "react";
+import UnsignedNav from "./unsigned-nav";
 
 
 const Home = () => {
 
     return (
+        <div className="home-container">
+            <UnsignedNav />
+            <div className="home-body">
+                <h1>Small React App</h1>
+            </div>
+
+        </div>
 
     )
 
