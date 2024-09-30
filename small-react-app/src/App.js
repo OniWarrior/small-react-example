0ae81d0e3b784a462869aabf6f55295d0d2c622d
Stephen Aranda <aranda.stephen88@gmail.com>
2024-09-29 20:08:58 -0500

    added css import

--- small-react-app/src/App.js
+++ small-react-app/src/App.js
@@ -1,9 +1,18 @@
 
 
+
+import Home from '../src/components/home'
+import React from 'react';
+import { Routes, Route } from 'react-router';
+import './styles/app.css'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
 
       </header>
     </div>
