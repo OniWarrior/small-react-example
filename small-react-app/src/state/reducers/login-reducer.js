f83646800c360d4e09bac018eb1e35be3e76aa4c
Stephen Aranda <aranda.stephen88@gmail.com>
2024-09-30 12:53:55 -0500

    created blank reducer

---
+++ small-react-app/src/state/reducers/login-reducer.js
@@ -0,0 +1,18 @@
+import {
+    LOGIN_START,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE
+} from '../actions/login-actions'
+
+const initialState = {
+    login: {},
+    loading: false,
+    error: ''
+
+}
+
+const loginReducer = (state = initialState, action) => {
+
+}
+
+export default loginReducer
