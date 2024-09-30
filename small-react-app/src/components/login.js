9822091e4612c63589b946f6096e5ded70c96672
Stephen Aranda <aranda.stephen88@gmail.com>
2024-09-29 21:12:24 -0500

    created state vars for navigate and login button

--- small-react-app/src/components/login.js
+++ small-react-app/src/components/login.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import UnsignedNav from "./unsigned-nav";
-
+import '../styles/login.css'
+import { useNavigate } from "react-router";
 
 
 const Login = () => {
+    const navigate = useNavigate()
+    //TODO add form schema
+    //const[login,errors,setLogin] =useValidation(LoginFormSchema)
+
+    const initialDisabled = true
+    const [disabled, setDisabled] = useState(initialDisabled)
+
+
+
+
 
     return (
         <div className="login-container">
             <UnsignedNav />
+
         </div>
     )
 
