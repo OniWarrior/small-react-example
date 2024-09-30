b42ca75fa06473c209e660dcd249ae856e902150
Stephen Aranda <aranda.stephen88@gmail.com>
2024-09-30 12:34:36 -0500

    completed successful response

---
+++ small-react-app/src/state/actions/login-actions.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+export const LOGIN_START = 'LOGIN_START'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAILURE = 'LOGIN_FAILURE'
+
+
+export const postLogin = (navigate, login) => (dispatch) => {
+    dispatch({ LOGIN_START })
+    //TODO GET endpoint once backend complete
+    axios.post('', login)
+        .then(response => {
+            dispatch({ LOGIN_SUCCESS, payload: response.data })
+            localStorage.setItem('token', response.data.token)
+            navigate('/user-dashboard')
+        })
+
+
+}
