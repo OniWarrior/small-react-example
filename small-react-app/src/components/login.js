cad2418641b23ebbe89b6f9b31074ace44fc2b3b
Stephen Aranda <aranda.stephen88@gmail.com>
2024-09-29 21:03:04 -0500

    added navigate and handler for going home

--- small-react-app/src/components/login.js
+++ small-react-app/src/components/login.js
@@ -1,8 +1,16 @@
 import React from "react";
 import UnsignedNav from "./unsigned-nav";
+import { useNavigate } from "react-router";
 
 
 const Login = () => {
+    const navigate = useNavigate()
+
+    const goHome = (e) => {
+        e.preventDefault()
+        navigate('/')
+    }
+
 
 }
 
