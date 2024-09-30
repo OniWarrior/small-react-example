0ac0c12935d0da0d3ca666741f8419d117be2abd
Stephen Aranda <aranda.stephen88@gmail.com>
2024-09-30 12:10:36 -0500

    completed setData and returned state values

--- small-react-app/src/hooks/useValidation.js
+++ small-react-app/src/hooks/useValidation.js
@@ -12,7 +12,30 @@ const initialErrors = {
 }
 
 
-const useValidation = () => {
+const useValidation = (schema) => {
+    const [data, setData] = useState(initialValues)
+    const [errors, setErrors] = useState(initialErrors)
+
+
+    const onInputChange = (event) => {
+        const { name, value } = event.target
+        yup.reach(schema, name)
+            .validate(value)
+            .then(() => {
+                setErrors({ ...errors, [name]: '' })
+            })
+            .catch(err => {
+                setErrors({ ...errors, [name]: err.errors[0] })
+            })
+        setData({
+            ...data,
+            [name]: value
+        })
+
+    }
+
+
+    return [data, errors, onInputChange]
 
 }
 
