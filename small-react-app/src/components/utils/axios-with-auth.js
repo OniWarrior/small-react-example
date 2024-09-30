4f66f000461a7a0ea97064ed0bc99afc308a45ac
Stephen Aranda <aranda.stephen88@gmail.com>
2024-09-30 12:18:20 -0500

    exported axios with auth

--- small-react-app/src/components/utils/axios-with-auth.js
+++ small-react-app/src/components/utils/axios-with-auth.js
@@ -2,4 +2,14 @@ import axios from 'axios'
 
 const axiosWithAuth = () => {
 
+    return axios.create({
+        // TODO: wait for backend endpoint
+        baseURL: '',
+        headers: {
+            authorization: localStorage.getItem('token')
+        }
+    })
+
 }
+
+export default axiosWithAuth
