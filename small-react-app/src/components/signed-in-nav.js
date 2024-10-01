837b4b8a3bbb91ca1677ec6b7764c554f8e96a1b
Stephen Aranda <aranda.stephen88@gmail.com>
2024-09-30 21:15:18 -0500

    created stylings for nav and body for dashboard

--- small-react-app/src/components/signed-in-nav.js
+++ small-react-app/src/components/signed-in-nav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router";
-import '../styles/unsigned-nav.css'
+import '../styles/nav.css'
 
 const SignedInNav = () => {
     const navigate = useNavigate()
@@ -15,7 +15,21 @@ const SignedInNav = () => {
         navigate('/login')
     }
 
+    const logout = (e) => {
+        e.preventDefault()
+        localStorage.removeItem('token')
+        navigate('/')
+    }
 
+    return (
+        <div className="nav-container">
+            <div className="row-nav">
+                <button className="button-home" onClick={goHome}>Home</button>
+                <button className="button-login" onClick={goLogin}>Login</button>
+                <button className="button-logout" onClick={logout}>Logout</button>
+            </div>
+        </div>
+    )
 
 
 
--- small-react-app/src/components/user-dashboard.js
+++ small-react-app/src/components/user-dashboard.js
@@ -1,11 +1,20 @@
 import React from "react";
 import SignedInNav from "./signed-in-nav";
+import '../styles/dashboard.css'
+
 
 const Dashboard = () => {
 
     return (
         <div className="dashboard">
             <SignedInNav />
+            <div className="dashboard-body">
+                <div className="dashboard-box">
+                    <h1>User Dashboard</h1>
+                    <p>Welcom to the user dashboard</p>
+                </div>
+
+            </div>
         </div>
     )
 
